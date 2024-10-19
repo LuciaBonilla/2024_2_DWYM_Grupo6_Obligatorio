05771143d920adab3dc20f101325a6730f7eac5e
Lucía Bonilla <lucia.bonilla@correo.ucu.edu.uy>
2024-10-18 22:27:54 -0300

    Ruteo y protección de pages

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,18 +1,104 @@
-import PublicRoutes from "./routes/PublicRoutes";
-import PrivateRoutes from "./routes/PrivateRoutes";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// RUTA PROTEGIDA.
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+// 9 PAGES.
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MyFeedPage from "./pages/MyFeedPage";
+import MyProfilePage from "./pages/MyProfilePage";
+import CreatePostPage from "./pages/CreatePostPage";
+import EditMyProfilePage from "./pages/EditMyProfilePage";
+import MyPostPage from "./pages/MyPostPage";
+import OtherUserProfilePage from "./pages/OtherUserProfilePage";
+import OtherUserPostPage from "./pages/OtherUserPostPage";
 
 /**
- * Objetivo del componente:
- * Cambiar la definición de rutas según si el usuario está autorizado.
- * @returns Definición de rutas.
+ * Define las rutas públicas y las rutas privadas.
+ * @estado TERMINADO.
  */
 function AppRouter() {
-  const { isAuthorizated } = useAuthContext();
-
   return (
     <>
-      <PublicRoutes />
-      {isAuthorizated && (<PrivateRoutes />)}
+      <Routes>
+        {/* Rutas públicas. */}
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+        {/* Redirige de "/" a "/login" directamente. */}
+        <Route
+          path="/"
+          element={<Navigate to="/login" />}
+        />
+        <Route
+          path="*"
+          element={<h1>Página no encontrada.</h1>}
+        />
+
+        {/* Rutas privadas. */}
+        <Route
+          path="/myfeed"
+          element={
+            <ProtectedRoute>
+              <MyFeedPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/createpost"
+          element={
+            <ProtectedRoute>
+              <CreatePostPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/myprofile"
+          element={
+            <ProtectedRoute>
+              <MyProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/myprofile/edit"
+          element={
+            <ProtectedRoute>
+              <EditMyProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/myposts/:id"
+          element={
+            <ProtectedRoute>
+              <MyPostPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profiles/:id"
+          element={
+            <ProtectedRoute>
+              <OtherUserProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/posts/:id"
+          element={
+            <ProtectedRoute>
+              <OtherUserPostPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
   )
 }
---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,25 @@
+import { Navigate } from "react-router-dom";
+
+// PROVEEDOR DE CONTEXTO.
+import { useAuthContext } from "../context-providers/AuthContextProvider";
+
+/**
+ * Protege una ruta privada no renderizando el contenido si el usuario no está autenticado.
+ * @param {*} children 
+ * @returns Si el usuario no está autenticado, entonces redirige a "/login". En caso contrario,
+ * renderiza el contenido protegido.
+ * @estado TERMINADO.
+ */
+function ProtectedRoute({ children }) {
+    const { isAuthorizated } = useAuthContext();
+
+    // Si no está autenticado, redirige a la page de login.
+    if (isAuthorizated === false) {
+        return <Navigate to="/login" />;
+    }
+
+    // Si está autenticado, renderiza el contenido.
+    return children;
+}
+
+export default ProtectedRoute;
