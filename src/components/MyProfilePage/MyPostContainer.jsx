636fb90fc9e5621fc48f4451457df6f1723c897e
GTula <guillotula@gmail.com>
2024-10-23 19:56:29 -0300

    my profile ready

---
+++ src/components/MyProfilePage/MyPostContainer.jsx
@@ -0,0 +1,110 @@
+import { createContext, useState } from "react";
+import { useAuthContext } from "../../context-providers/AuthContextProvider";
+import { useEffect } from "react";
+import '../../styles/mobile/MyProfilePage.css'
+
+
+// CLASES AUXILIARES.
+import BackendCaller from "../../auxiliar-classes/BackendCaller";
+
+// COMPONENTES.
+import PostCard from "./MyPostCard";
+
+
+
+function PostsContainer(){
+    const {username} = useAuthContext();
+
+    // Posts a mostrar.
+    const [posts, setPosts] = useState([]);
+
+    // Indica que está cargando.
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Necesarios para obtener los posts y filtrar los posts.
+    const { token, userID } = useAuthContext();
+
+    /**
+     * Dados los posts obtenidos, quita los posts propios del usuario y los restantes los ordena cronológicamente.
+     * @param posts
+     */
+    function getMyFeed(posts) {
+        // Filtra los posts para eliminar los del usuario actual
+        const feed = posts.filter((post) => post.user._id == userID);
+
+        // Ordena los posts por fecha de forma descendente (más recientes primero)
+        return feed.sort((post1, post2) => new Date(post2.createdAt) - new Date(post1.createdAt));
+    }
+
+    /**
+     * Obtiene el feed del usuario.
+     */
+    async function fetchFeed() {
+        const response = await BackendCaller.getFeed(token);
+
+        if (response.statusCode === 200) { // OK
+            setPosts(getMyFeed(response.data));
+        }
+    }
+
+    useEffect(() => {
+        fetchFeed();
+        setIsLoading(false);
+    }, []) // Ejecuta cuando se renderiza el componente.
+
+    if (isLoading) {
+        return (<p className="loading-message">CARGANDO...</p>);
+    }
+
+    return (
+        <div >
+            <h1 className="my-feed-page__social-network-title">{username}</h1>
+            <div className="my-profile-container">
+                <div className="profile-photo-container">
+                    <img className='short-profile-card__img' src=".\src\assets\default_profile.png" alt="Profile"/>
+                    
+                    <div className="profile-stats-container">
+                        <div className="profile-stat">
+                            <p className="stat-number">{posts.length}</p>
+                            <p className="stat-label">Posts</p>
+                        </div>
+                        <div className="profile-stat">
+                            <p className="stat-number">100</p>
+                            <p className="stat-label">Followers</p>
+                        </div>
+                        <div className="profile-stat">
+                            <p className="stat-number">100</p>
+                            <p className="stat-label">Following</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="buttons-profile">
+                    <button className='profile-log-out-button'>Cerrar Sesión</button>
+                    <button className='profile-log-out-button'>Editar Perfil</button>
+                </div>
+
+                <article className="instagram-profile">
+                    {posts.length > 0 ? (
+                        posts.map((post) => (
+                            <PostCard
+                                key={post._id}
+                                id={post._id}
+                                user={post.user}
+                                imageURL={post.imageUrl}
+                                caption={post.caption}
+                                comments={post.comments}
+                                likes={post.likes}
+                                createdAt={post.createdAt}
+                                fetchFeed={fetchFeed}
+                            />
+                        ))
+                    ) : (
+                        <p className="post-card-container__no-posts-message">NO HAY POSTS</p>
+                    )}
+                </article>
+            </div>
+        </div>
+    );
+}
+
+export default PostsContainer;
--- src/pages/MyProfilePage.jsx
+++ src/pages/MyProfilePage.jsx
@@ -1,7 +1,18 @@
+import { useAuthContext } from '../context-providers/AuthContextProvider';
+import '../styles/mobile/MyProfilePage.css'
+import PostsContainer from "../components/MyProfilePage/MyPostContainer";
+import Navbar from "../components/shared-components/Navbar";
+
 function MyProfilePage() {
+    
     return (
-        <h1>My Profile Page</h1>
+        <div>
+            
+            <div><PostsContainer/></div>
+            <Navbar />
+        </div>
     );
+
 }
 
 export default MyProfilePage;
