266203aa56a8ddfbe25e969b32c4848f504f0711
Lucía Bonilla <lucia.bonilla@correo.ucu.edu.uy>
2024-11-03 07:22:12 -0300

    Componentes de EditMyProfilePage terminados

---
+++ src/components/EditMyProfilePage/AttributeToEditMenu.jsx
@@ -0,0 +1,15 @@
+/**
+ * Menú para seleccionar un atributo de perfil a editar.
+ * @param {*} handleShowEditMyProfileForm
+ * @estado TERMINADO.
+ */
+function AttributeToEditMenu({ handleShowEditMyProfileForm }) {
+    return (
+        <section className="edit-my-profile-menu">
+            <button className="edit-my-profile-menu__option-button" onClick={() => handleShowEditMyProfileForm("username")}>EDITAR NOMBRE DE USUARIO</button>
+            <button className="edit-my-profile-menu__option-button" onClick={() => handleShowEditMyProfileForm("profilePicture")}>EDITAR FOTO DE PERFIL</button>
+        </section>
+    )
+}
+
+export default AttributeToEditMenu;
---
+++ src/components/EditMyProfilePage/MyProfileCard.jsx
@@ -0,0 +1,22 @@
+// IMAGES.
+import defaultPhoto from "../../assets/default_profile.png";
+
+// CLASES AUXILIARES.
+import Base64Converter from "../../auxiliar-classes/Base64Converter";
+
+/**
+ * Tarjeta de perfil al editar perfil.
+ * @param {*} username 
+ * @param {*} profilePicture 
+ * @estado TERMINADO.
+ */
+function MyProfileCard({ username, profilePicture }) {
+    return (
+        <article className="edit-my-profile-card">
+            <img className="edit-my-profile-card__profile-picture" src={profilePicture === "" ? defaultPhoto : Base64Converter.checkBase64Image(profilePicture)} />
+            <h2 className="edit-my-profile-card__username">{username}</h2>
+        </article>
+    )
+}
+
+export default MyProfileCard;
